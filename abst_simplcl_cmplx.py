30e85df46c4dc3cc07411890540d1a7557892803
rpadmanabhan <raghus606@gmail.com>
2021-02-17 17:19:09 -0500

    Added some comments and ideas

--- abst_simplcl_cmplx.py
+++ abst_simplcl_cmplx.py
@@ -9,10 +9,11 @@ class Vertex:
     def __init__(self, *args, **kwargs):
         '''
         '''
-        self.label       = kwargs["label"]
-        self.parent      = kwargs.get("parent", None)
-        self.level       = kwargs.get("level", None)
-        self.connections = kwargs.get("connections", set())
+        self.label            = kwargs["label"]
+        self.parent           = kwargs.get("parent", None)
+        self.level            = kwargs.get("level", None)
+        self.connections      = kwargs.get("connections", set()) # child connections
+        self.tree_connections = kwargs.get("tree_connections", set()) # connections to edges in another part of the tree
 
     def __repr__(self):
         '''
@@ -43,6 +44,10 @@ class ASC:
         '''
         vertex.parent = parent
         parent.connections.add(vertex)
+        # mark a connection to this child vertex from the same labeled vertex at the root level., i.e. level0 to leveln connection
+        if parent.label != "root":
+            self.vertex_tracker[("root", vertex.label, 0)].tree_connections.add(vertex)
+        # track this vertex by some key - not sure if this is the best way
         self.vertex_tracker[(parent.label, vertex.label, vertex.level)] = vertex
 
 
@@ -68,7 +73,7 @@ class ASC:
 
 
     def _get_simplices(self, n, simplices, parent = None, labels = []):
-        ''' recursive method to retrieve labels asscoiated with all n-simplices under a parent
+        ''' recursive method to retrieve labels associated with all n-simplices under a parent
         :param int: n: dimension of the simplex
         :param list: simplices: container for storing any n-simplex found
         :param Vertex: parent: Parent vertex to start the search under
@@ -92,12 +97,13 @@ class ASC:
             labels.pop()
 
 
+    ## TO DO: extend to allow filter by label/object
     def ret_all_simplices(self, n):
         ''' public method to return all n-simplices
         :param int: n: Return labels associated with all simplices of dimension n in this ASC
         '''
         simplices = []
-        seen     = set()
+        seen      = set()
         for i in range(n, self.max_level + 1):
             for parent in self.level_tracker[i]:
                 if parent.label in seen:
@@ -107,3 +113,24 @@ class ASC:
                 self._get_simplices(n, simplices, parent, labels)
 
         return simplices
+
+
+
+
+######################################################################################################################################
+## Some ideas/notes for future.
+
+## 1.
+## Abstract a Simplex as a tree like it is being done in the ASC class and consider the Abstract Simplicial Complex as a Graph
+## with each Node as the level 0 node of a Simplex (i.e. a point/vertex) and Edges between them as connections between a point/vertex to another simplex.
+## e.g. The 2-simplex A->B->C is say, SimplexX and stored as a tree with level 0 at A;
+##      The 1-simplex B->C is say, SimplexY and also stored as a tree with level 0 at B.
+## Then the Abstract Simplicial Complex is a Graph like : [SimplexY]---B-@-level0---B-@-level1--->[SimplexX]
+## I am thinking only a directed edge from the level0 node to some levelN node is needed. Since the faces are captured in the Simplex itself.
+## This allows for queries like : Give me all n-simplices which have the object/label B in it.
+
+## Action to take: Break away tree representation of a Simplex from ASC into a seperate class:
+#class Simplex:
+#    pass
+
+
