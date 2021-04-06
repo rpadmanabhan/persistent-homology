f4661a508ef493dc85951bec0662ab2997d4cf71
rpadmanabhan <raghus606@gmail.com>
2021-04-06 16:54:46 -0400

    Add func for computing basis for ker, img and homology

--- matrix_ops.py
+++ matrix_ops.py
@@ -80,7 +80,7 @@ def reduced_row_echelon_Z2(A, bounds = None):
         ## Find pivot column
         pivot_col = None
         for j in range(start_col - 1, end_col):
-            if A[i][j] == 1:
+            if A[i, j]:
                 pivot_col = j
                 break
         assert pivot_col is not None, "Bug ! Could not find pivot in row: {}\nrow_idx: {}".format(A[i], i)
@@ -118,30 +118,75 @@ def smith_normal_form(A):
     :rtype: tuple of (rank_A, np.array(dtype=np.uint8), np.array(dtype=np.uint8), np.array(dtype=np.uint8))
     '''
 
-    # Followed algorithm from here :
-    # http://people.maths.ox.ac.uk/nanda/cat/Lecture%2003%20Homology.pdf
+    ## Followed algorithm from here :
+    ## http://people.maths.ox.ac.uk/nanda/cat/Lecture%2003%20Homology.pdf
     #--------------------------------------------------------------------------------------
-    # B = I_nxn  A
-    #     0_mxn  I_mxm
-    # Note: 0_mxn will never get modified.
+    ## B = I_nxn  A
+    ##     0_mxn  I_mxm
+    ## Note: 0_mxn will never get modified.
     nrows, ncols = A.shape
     B = np.block(
         [[np.eye(nrows, dtype = np.uint8), A],
-         [np.zeros(shape = (nrows, ncols), dtype = np.uint8),
+         [np.zeros(shape = (ncols, nrows), dtype = np.uint8),
           np.eye(ncols, dtype = np.uint8)]])
-    # Use Row/Col ops to transform A to a diagonal matrix
+    ## Use Row/Col ops to transform A to a diagonal matrix
     rank_A = row_echelon_Z2(B, (1, nrows, nrows + 1, nrows + ncols), reduce_cols = True)
 
-    P = B[0: nrows, 0: ncols]
+    P = B[0: nrows, 0: nrows]
     Q = B[nrows:, nrows:]
     D = B[0: nrows:, nrows:]
+    ## Note:
+    ## basis for im(A) corresponds to the first rank_A cols for P_inv
+    ## basis for ker(A) corresponds to the last (n - rank_A) cols of Q
     return (rank_A, inverse_Z2(P), D, Q)
 
 
-def homology(b_k, b_k1):
-    ''' Compute basis for the kth homology group
-    :param b_k: Boundary matrix for C_k
-    :param b_k1: Boundary matrix for C_k+1
+def basis_img_ker_Z2(d):
+    ''' Compute basis for the image/kernel of a boundary matrix
     '''
-    pass
+    nrows, ncols = d.shape
+    ## Smith Normal Form for the boundary matrix
+    rank, P_inv, D, Q = smith_normal_form(d)
+    ## return basis for image - first rank cols of P_inv and kernel - last nrows - rank cols of Q
+    return P_inv[:, :rank], Q[:, rank:]
 
+
+def basis_homology_Z2(d_k, d_k1):
+    ''' Compute basis for the kth homology group
+    :param d_k: Boundary matrix for C_k
+    :param d_k1: Boundary matrix for C_k+1
+    '''
+    nrows_k, ncols_k = d_k.shape
+
+    ## Form a block matrix with basis vectors of the img of C_k+1 and ker of C_k
+    ## G_k = [B_k | Z_k]
+    img_d_k, ker_d_k = basis_img_ker_Z2(d_k)
+    img_d_k1, ker_d_k1 = basis_img_ker_Z2(d_k1)
+    rank_ker_d_k = ker_d_k.shape[1]
+    rank_img_d_k1 = img_d_k1.shape[1]
+    if rank_img_d_k1 == 0:
+        img_d_k1 = np.array(
+            img_d_k1, dtype=np.uint8).reshape(ker_d_k.shape[0], 0)
+    G_k = np.hstack([img_d_k1, ker_d_k])
+    ## Compute reduced row echelon form of G_k
+    reduced_row_echelon_Z2(G_k)
+    ## E_k = reduced_row_echelon(G_k) = [B'_k | Z'_k]
+
+    ## Find pivot columns of Z'_k
+    ## Start from the bottom row of E_k, the last rank_k - rank_k1 pivot cols is our desired answer
+    nrows_G_k, ncols_G_k = G_k.shape
+    pivot_cols = []
+    for i in range(nrows_G_k - 1, -1, -1):
+        ## Find the pivot column
+        for j in range(0, ncols_G_k):
+            if G_k[i, j]:
+                ## We need the col number relative to ker_d_k sub-block
+                pivot_col = rank_img_d_k1 - j
+                assert pivot_col >= 0
+                pivot_cols.append(pivot_col)
+                break
+
+        if len(pivot_cols) == rank_ker_d_k - rank_img_d_k1: break
+
+    ## Corresponding columns in Z_k form a basis for H_k(C_k, d_k)
+    return ker_d_k[:, pivot_cols]
--- tests/test_matrix_ops.py
+++ tests/test_matrix_ops.py
@@ -48,6 +48,7 @@ class TestReducedRowEchelon(unittest.TestCase):
                        [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
                       dtype = np.uint8)).all(), True)
 
+
 class TestInverse(unittest.TestCase):
     '''
     '''
@@ -61,6 +62,7 @@ class TestInverse(unittest.TestCase):
             (matrix_ops.inverse_Z2(A) ==
              np.array([[1, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1]],
                       dtype = np.uint8)).all(), True)
+
 
 class TestSmithNormalForm(unittest.TestCase):
     '''
@@ -85,6 +87,44 @@ class TestSmithNormalForm(unittest.TestCase):
             (Q ==
              np.array([[1, 0, 0, 1], [0, 1, 0, 1],
                        [0, 0, 1, 1], [0, 0, 0, 1]])).all(), True)
+
+
+class TestBasisImKer(unittest.TestCase):
+    '''
+    '''
+    def test_basis_img_ker_Z2(self):
+        ''' Checks for correct computation of the basis for images and kernels
+        '''
+        A = np.array(
+            [[1, 0, 0, 1], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1]],
+            dtype = np.uint8)
+        im, ker = matrix_ops.basis_img_ker_Z2(A)
+        self.assertEqual(
+            (im == np.array([[1, 0, 0], [1, 1, 0],
+                             [0, 1, 1], [0, 0, 1]], dtype = np.uint8)).all(), True)
+        self.assertEqual(
+            (ker == np.array([[1], [1], [1], [1]], dtype = np.uint8)).all(), True)
+
+
+class TestHomology(unittest.TestCase):
+    '''
+    '''
+    def test_homology_Z2(self):
+        ''' Checks for correct computation of the basis for homology
+        '''
+        d_0 = np.array([[0, 0, 0, 0]], dtype = np.uint8)
+        d_1 = np.array(
+            [[1, 0, 0, 1], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1]],
+            dtype = np.uint8)
+
+        basis_H0 = matrix_ops.basis_homology_Z2(d_0, d_1)
+        self.assertEqual(
+            (basis_H0 == np.array([[1], [0], [0], [0]], dtype = np.uint8)).all(), True)
+
+        d_2 = np.array([[0]], dtype = np.uint8)
+        basis_H1 = matrix_ops.basis_homology_Z2(d_1, d_2)
+        self.assertEqual(
+            (basis_H1 == np.array([[1], [1], [1], [1]], dtype = np.uint8)).all(), True)
 
 
 if __name__ == '__main__':
